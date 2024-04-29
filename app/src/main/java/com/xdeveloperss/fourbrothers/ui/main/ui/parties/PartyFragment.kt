371d6793f2409ded48e53943990c5a0813896248
Anas Sharif <20491569+AnasSharif@users.noreply.github.com>
2024-04-30 00:18:46 +0500

    Add search for person

--- app/src/main/java/com/xdeveloperss/fourbrothers/ui/main/ui/parties/PartyFragment.kt
+++ app/src/main/java/com/xdeveloperss/fourbrothers/ui/main/ui/parties/PartyFragment.kt
@@ -1,8 +1,19 @@
 package com.xdeveloperss.fourbrothers.ui.main.ui.parties
 
+import android.app.SearchManager
+import android.content.Context
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
 import com.kongzue.dialogx.dialogs.WaitDialog
+import com.xdeveloperss.fourbrothers.R
 import com.xdeveloperss.fourbrothers.adapters.AdapterType
 import com.xdeveloperss.fourbrothers.adapters.GenericAdapter
+import com.xdeveloperss.fourbrothers.data.models.Person
 import com.xdeveloperss.fourbrothers.databinding.FragmentPartyBinding
 import com.xdeveloperss.fourbrothers.xbase.XBaseFragment
 import com.xdeveloperss.fourbrothers.xnetwork.config.response.getValueFromResponse
@@ -12,19 +23,57 @@ import java.util.Date
 class PartyFragment : XBaseFragment<FragmentPartyBinding>(FragmentPartyBinding::inflate) {
 
     private val partyViewModel: PartyViewModel by sharedViewModel()
+    private var parties: List<Person> = listOf()
     override fun onViewCreated() {
 
         partyViewModel.getData.observe { response->
             response.getValueFromResponse()?.data?.let {
-                binding.partiesRV.adapter = GenericAdapter(type = AdapterType.PERSON, it.persons)
-                { i, action, person ->
-                }
+                parties = it.persons
+                setupAdapter(parties)
             }
         }
         this.loadData()
+        this.searchBarView()
+    }
+    private fun setupAdapter(persons: List<Person>){
+        binding.partiesRV.adapter = GenericAdapter(type = AdapterType.PERSON, persons)
+        { i, action, person ->
+
+        }
     }
     private fun loadData(){
         WaitDialog.show("Load Data...")
         partyViewModel.setData(types = listOf("persons"))
+    }
+
+    private fun searchBarView(){
+
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.searchable_menu, menu)
+            }
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                val searchManager =
+                    requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+                menuItem.actionView?.let {
+                    val searchView = it as SearchView
+                    searchView.setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+                    val queryTextListener = object : SearchView.OnQueryTextListener {
+                        override fun onQueryTextChange(newText: String?): Boolean {
+                            val pp =  parties.filter { it.name.lowercase().contains(newText.toString().lowercase()) }.toMutableList()
+                            setupAdapter(pp)
+                            return true
+                        }
+
+                        override fun onQueryTextSubmit(query: String?): Boolean {
+                            return true
+                        }
+                    }
+                    searchView.setOnQueryTextListener(queryTextListener)
+                }
+                return false
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
