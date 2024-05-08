3271fd0269d6f44f130be521001b7c67826a44f3
Anas Sharif <20491569+AnasSharif@users.noreply.github.com>
2024-05-08 16:05:17 +0500

    Add employee module

---
+++ app/src/main/java/com/xdeveloperss/fourbrothers/ui/main/ui/employee/EmployeeFragment.kt
@@ -0,0 +1,10 @@
+package com.xdeveloperss.fourbrothers.ui.main.ui.employee
+
+import com.xdeveloperss.fourbrothers.databinding.FragmentEmployeeBinding
+import com.xdeveloperss.fourbrothers.xbase.XBaseFragment
+
+class EmployeeFragment: XBaseFragment<FragmentEmployeeBinding>(FragmentEmployeeBinding::inflate)  {
+    override fun onViewCreated() {
+    }
+
+}
